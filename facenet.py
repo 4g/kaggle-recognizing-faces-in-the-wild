c5cd66d1a0a95506bfd3fb0e80e04dbb7586a7ff
4g <apurvagup@gmail.com>
2019-05-15 23:37:03 +0530

    facenet 0.743

---
+++ facenet.py
@@ -0,0 +1,45 @@
+from keras_facenet import FaceNet
+from utils import load_data
+from scipy.spatial.distance import euclidean
+from sklearn.preprocessing import normalize
+import numpy as np
+from tqdm import tqdm
+
+train_images, train_labels, test_images, test_labels, train_pairs, submission_pairs = load_data()
+
+embedder = FaceNet()
+embeddings = embedder.embeddings(test_images)
+embeddings = normalize(embeddings)
+
+print(embeddings.shape)
+
+test_labels_indices = {l: i for i, l in enumerate(test_labels)}
+
+distances = []
+
+for pair in tqdm(submission_pairs):
+	p1, p2 = pair
+	i1 = test_labels_indices[p1]
+	i2 = test_labels_indices[p2]
+	e1 = embeddings[i1]
+	e2 = embeddings[i2]
+	dist = euclidean(e1, e2)
+	distances.append(dist)
+
+# convert distances to probability distribution
+distances = np.asarray(distances)
+s_dist = np.sum(distances)
+p_related = []
+for distance in distances:
+	p_dist = 1 - np.sum(distances[np.where(distances <= distance)[0]]) / s_dist
+	p_related.append(p_dist)
+
+
+output_csv = open("data/output.csv", 'w')
+output_csv.write("img_pair,is_related\n")
+
+for pair, prel in zip(submission_pairs, p_related):
+	s = "{pair},{prel}\n".format(pair="-".join(pair), prel=str(prel))
+	output_csv.write(s)
+
+output_csv.close()
---
+++ utils.py
@@ -0,0 +1,53 @@
+import glob
+from tqdm import tqdm
+import cv2
+from keras.preprocessing import image as imglib
+import numpy as np
+
+train_images_path = "data/train/*"
+test_images_path = "data/test/*.jpg"
+train_pairs_path = "data/train_relationships.csv"
+submission_pairs_path = "data/sample_submission.csv"
+
+
+def load_data():
+	_train_images = []
+	_train_labels = []
+
+	for family in tqdm(glob.glob(train_images_path)):
+		for members in glob.glob(family + "/*"):
+			for image in glob.glob(members + "/*.jpg"):
+				_train_images.append(cv2.imread(image))
+				label = "/".join(members.split("/")[-2:])
+				_train_labels.append(label)
+
+	_test_images = []
+	_test_labels = []
+	for image_path in tqdm(glob.glob(test_images_path)):
+		image = imglib.load_img(image_path)
+		image = imglib.img_to_array(image, dtype=np.float32)
+		_test_images.append(image)
+		label = "/".join(image_path.split("/")[-1:])
+
+		_test_labels.append(label)
+
+	_train_pairs = [i.strip().split(",") for i in open(train_pairs_path)][1:]
+	submission_pairs = [i.strip().split(",")[0].split("-") for i in open(submission_pairs_path)][1:]
+
+	label_counts = {}
+
+	for label in _train_labels:
+		label_counts[label] = label_counts.get(label, 0) + 1
+
+	pair_count = 0
+	for l1, l2 in _train_pairs:
+		pair_count += label_counts.get(l1, 0) * label_counts.get(l2, 0)
+
+	print("Num image pairs", pair_count)
+	print("Num train images", len(_train_images))
+	print("Num test images", len(_test_images))
+	print("Num person pairs", len(_train_pairs))
+	print("Num submission pairs", len(submission_pairs))
+
+	return _train_images, _train_labels, _test_images, _test_labels, _train_pairs, submission_pairs
+
